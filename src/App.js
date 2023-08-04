961d0546143295242b333284627329c88517d994
Gabriel Scholze Rosa <gabrielscholze24@gmail.com>
2023-08-03 21:11:58 -0300

    filtering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { getDataEn, getDataPt } from './config/FilterData'; 
+import { useEffect, useState } from 'react';
 function App() {
+  const [dataEn, setEn] = useState(null)
+  useEffect(() => {
+    setEn(getDataEn())
+    console.log(dataEn)
+  },[])
+  useEffect(() => {
+    console.log(dataEn)
+  },[dataEn])
   return (
     <div className="App">
-      
-    </div>
+        loldele
+    </div>  
   );
 }
 
--- src/config/FilterData.js
+++ src/config/FilterData.js
@@ -1,22 +1,52 @@
-import DATA_english from "../../public/data/DATA_enus.json"
-import DATA_portuguese from "../../public/data/DATA_ptbr.json"
+import DATA_english from "../data/DATA_enus.json"
+import DATA_portuguese from "../data/DATA_ptbr.json"
 
 const getDataEn = () => {
     const filteredData = {};
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            const item = data[key];
-            filteredData[key] = {
-                id: key,
-                name: item.name,
-                description: item.description,
-                plaintext: item.plaintext,
-                image: item.image,
-                gold: item.gold,
-                stats: item.stats
-            };
+    let key = 0;
+    for (const itemId in DATA_english.data) {
+        if (DATA_portuguese.data.hasOwnProperty(itemId) && DATA_portuguese.data["name"] !== " ") {
+            const item = DATA_english.data[itemId];
+            if (item.name.trim() !== "") {
+                filteredData[key] = {
+                    id: itemId,
+                    name: item.name,
+                    description: item.description,
+                    plaintext: item.plaintext,
+                    image: item.image,
+                    gold: item.gold,
+                    stats: item.stats,
+                };
+                key++;
+            }
         }
     }
     return filteredData;
-    
+};
+const getDataPt = () => {
+    const filteredData = {};
+    let key = 0;
+    for (const itemId in DATA_portuguese.data) {
+        if (DATA_portuguese.data.hasOwnProperty(itemId)) {
+            const item = DATA_english.data[itemId];
+            if (item.name.trim() !== "") {
+                filteredData[key] = {
+                    id: itemId,
+                    name: item.name,
+                    description: item.description,
+                    plaintext: item.plaintext,
+                    image: item.image,
+                    gold: item.gold,
+                    stats: item.stats,
+                };
+                key++;
+            }
+        }
+    }
+    return filteredData;
+};
+
+export {
+    getDataEn,
+    getDataPt
 }
